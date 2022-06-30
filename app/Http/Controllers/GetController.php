fab6fdc731220d6c1c983747b7f59d605ecf0c80
San4a66 <e@w.com>
2022-06-30 19:19:17 +0500

    ADD GetController

---
+++ app/Http/Controllers/GetController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class GetController extends Controller
+{
+    public function __invoke(){
+        return 1111;
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['middleware' => 'auth:sanctum'], function() {
+
+    Route::get('get', \App\Http\Controllers\GetController::class );
+});
+
+
